39ef44fbca5f68ea22a6a7f0e4f6fe2f059a3d44
Stephane <jiekakc3@gmail.com>
2026-06-22 21:55:25 +0100

    firt commit

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/mon-portefeuille-pwa/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  base: "/mon-portefeuille-pwa",
 })
