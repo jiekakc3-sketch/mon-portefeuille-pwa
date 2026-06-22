8f7f6bb9df563cffc91280949e65952284d28100
Stephane <jiekakc3@gmail.com>
2026-06-22 22:21:01 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'pwa-wallet-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2489/2489756.png'
+];
+
+// 1. Installazione: salviamo i file essenziali nella cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 2. Attivazione: pulizia di vecchie cache se presenti
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 3. Strategia di Fetch: Cache First, poi Network (ottimo per la velocità offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
